34f82f4e15794e59ed424f35ec7363026706d246
tomas elias <eliast@avaya.com>
2020-05-20 19:27:40 -0300

    Ej. Dimensiones Fijas

--- App.js
+++ App.js
@@ -30,56 +30,18 @@ export default class App extends Component {
 
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        {this.state.value ? (
-          <Text style={styles.welcome}>{this.state.value}</Text>
-        ) : (
-          <Text style={styles.welcome}>Welcome to React Native!</Text>
-        )}
-        <Image
-          source={require("./assets/ReactNative.png")}
-          style={styles.image}
-        />
-        <TextInput
-          style={{
-            height: 40,
-            width: 200,
-            borderColor: "#AAAAAA",
-            borderWidth: 2,
-          }}
-          onChangeText={(text) => this.setState({ value: text })}
-          value={this.state.value}
-        />
-        <View style={styles.buttonsContainer}>
-          <Button name="Sumar" onPress={this.sumar} />
-          <Button name="Restar" onPress={this.restar} />
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View
+            style={{ width: 100, height: 100, backgroundColor: "powderblue" }}
+          />
+          <View
+            style={{ width: 250, height: 250, backgroundColor: "skyblue" }}
+          />
+          <View
+            style={{ width: 350, height: 350, backgroundColor: "steelblue" }}
+          />
         </View>
-        <View style={styles.countContainer}>
-          <Text style={styles.countText}>{this.state.count}</Text>
-        </View>
-        <ScrollView
-          style={{
-            marginTop: 30,
-            width: 200,
-          }}
-        >
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum.
-          </Text>
-        </ScrollView>
       </SafeAreaView>
     );
   }
@@ -88,8 +50,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#FFFFFF",
   },
   welcome: {
