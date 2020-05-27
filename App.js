2457e7e1dfdc011291fdedfbba72c78173be2fd9
tomas elias <eliast@avaya.com>
2020-05-27 08:53:56 -0300

    FlatList

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from "react-native";
+import { DATA } from "./src/data/data";
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  item: {
+    backgroundColor: "#444444",
+    padding: 20,
+    marginVertical: 5,
+    marginHorizontal: 5,
+  },
+  title: {
+    fontSize: 32,
+    color: "#DDDDDD",
   },
 });
---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+export const DATA = [
+  {
+    id: "01",
+    title: "Item 1",
+  },
+  {
+    id: "02",
+    title: "Item 2",
+  },
+  {
+    id: "03",
+    title: "Item 3",
+  },
+  {
+    id: "04",
+    title: "Item 4",
+  },
+  {
+    id: "05",
+    title: "Item 5",
+  },
+  {
+    id: "06",
+    title: "Item 6",
+  },
+  {
+    id: "07",
+    title: "Item 7",
+  },
+  {
+    id: "08",
+    title: "Item 8",
+  },
+  {
+    id: "09",
+    title: "Item 9",
+  },
+  {
+    id: "10",
+    title: "Item 10",
+  },
+];
